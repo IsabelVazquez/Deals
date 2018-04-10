1bb98c624676732dc9c2b2138058a35f5c7138d1
github username <12ivazquez@gmail.com>
2018-04-10 15:40:20 -0500

    Added green palette and starting text

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'yellowgreen'}}>
+          <Text>Username or Welcome, guest</Text>
+        </View>
+        <View style={{flex: 2, backgroundColor: 'mediumspringgreen'}}>
+          <Text>Latest deal in the area, must ask for location</Text>
+        </View>
+        <View style={{flex: 3, backgroundColor: 'lightgreen'}} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
